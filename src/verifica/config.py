22931211336306471322675a2a9f555862171646
espinafr <theo.ruela@gmail.com>
2026-08-13 16:59:05 -0300

    Adiciona docstrings para classes e métodos

--- src/verifica/config.py
+++ src/verifica/config.py
@@ -1,19 +1,33 @@
 from platformdirs import user_config_dir
 from importlib.metadata import version, PackageNotFoundError
 from time import time
+import logging
 import json
 import os
 
+logger = logging.getLogger(__name__)
 
 class Config:
+    """Classe de configurações compartilhadas
+
+    Returns:
+        _type_: True se a instância foi criada, False caso contrário
+    """
     _instance = None
 
     def __new__(cls, *args, **kwargs):
+        """Gerador de instância, impede que Config seja iniciado mais de uma vez
+
+        Returns:
+            bool: True se a instância foi criada, False caso contrário
+        """
         if cls._instance is None:
             cls._instance = super().__new__(cls, *args, **kwargs)
         return cls._instance
 
     def __init__(self):
+        """Inicializa a classe
+        """
         if hasattr(self, "_initialized"):
             return
         self._initialized = True
@@ -24,12 +38,23 @@ class Config:
         self.config_dir = config_dir
         self.config_path = os.path.join(config_dir, "config.toml")
         self.__check_config_state()
+        logger.info("Configurações inicializadas.")
 
     def __save_keys(self, data: dict):
+        """Salva as configurações da memória para o arquivo de configuração
+
+        Args:
+            data (dict): Configurações a serem salvas
+        """
         with open(self.config_path, "w", encoding="utf-8") as f:
             json.dump(data, f, indent=4)
 
     def __read_keys(self) -> dict:
+        """Lê o arquivo de configurações
+
+        Returns:
+            dict: Configurações lidas do arquivo
+        """
         if not self.config_path or not os.path.exists(self.config_path):
             return {}
         with open(self.config_path, "r", encoding="utf-8") as f:
@@ -39,6 +64,8 @@ class Config:
                 return {}
 
     def __check_config_state(self):
+        """Checa se o arquivo de configurações está com conteúdo e atualizado
+        """
         defaultConfig = {
             "version": self.get_version(),
             "url": "https://raw.githubusercontent.com",
@@ -51,8 +78,14 @@ class Config:
         else:
             if current_config.get("version") != defaultConfig.get("version"):
                 self.update_config(defaultConfig, defaultConfig.get("version"))
+        logger.info("Configurações verificadas e atualizadas.")
 
     def get_version(self):
+        """Obtém a versão da aplicação
+
+        Returns:
+            str: Versão da aplicação
+        """
         try:
             __version__  = version("verifica")
         except PackageNotFoundError:
@@ -60,6 +93,12 @@ class Config:
         return __version__
 
     def update_config(self, new_data: dict, version: str = None):
+        """Atualiza uma configuração específica
+
+        Args:
+            new_data (dict): Dicionário contendo configurações a serem salvas
+            version (str, optional): Versão da aplicação. O padrão é None.
+        """
         current_config = self.__read_keys()
         if version:
             current_config["version"] = version
@@ -67,12 +106,28 @@ class Config:
         self.__save_keys(new_data)
 
     def get_config(self, key: str = None) -> dict:
+        """Pega uma configuração específica do arquivo de configurações, ou todas as configurações se nenhuma chave for passada
+
+        Args:
+            key (str, optional): Configuração desejada. O padrão é None.
+
+        Returns:
+            dict: Configurações lidas do arquivo
+        """
         config = self.__read_keys()
         if key:
+            logger.info(f"Configuração {key} obtida.")
             return config.get(key, "")
+        logger.info(f"Obtendo todas as configurações.")
         return config
 
     def set_config(self, key: str, value):
+        """Altera uma configuração
+
+        Args:
+            key (str): Configuração a ser alterada
+            value: Valor novo
+        """
         config = self.__read_keys()
         config[key] = value
         self.update_config(config)
--- src/verifica/fetcher.py
+++ src/verifica/fetcher.py
@@ -9,6 +9,12 @@ from .config import settings
 
 class Fetcher:
     def __init__(self, path, local: bool = False):
+        """Inicializa uma nova instância da classe
+
+        Args:
+            path (_type_): Caminho do arquivo de respostas ou URL do exercício
+            local (bool, optional): Indica se o arquivo de respostas é local. O padrão é False.
+        """
         self.exercise = path
         self.local = local
         if not local:
@@ -17,17 +23,29 @@ class Fetcher:
         self.logger = logging.getLogger(__name__)
 
     def __str__(self):
-        return f"Fetcher(url=\"{self._build_url()}\")"
+        """Representação em string da classe
 
-    def _build_url(self):
+        Returns:
+            _type_: String representando a instância da classe
+        """
+        return f"Fetcher(path=\"{self.path}\", local={self.local})"
+
+    def _build_url(self) -> str:
+        """Constroi a URL completa para o arquivo de respostas.
+
+        Returns:
+            str: URL formatada
+        """
         return f"{self.base_url}/{self.remote_path}"
 
     def fetch(self) -> str:
         """Baixa o arquivo de respostas e salva em uma pasta temporária
 
-        :returns: O caminho do arquivo salvo
-        :rtype: str
-        :raises RuntimeError: Se não for possível buscar o exercício
+        Raises:
+            RuntimeError: Se não for possível buscar o exercício
+
+        Returns:
+            str: O caminho do arquivo salvo
         """
         try:
             self.logger.info("Baixando arquivo de correção...")
@@ -48,16 +66,34 @@ class Fetcher:
 
         return self.file.name
 
-    def get_file(self):
+    def get_file(self) -> str:
+        """Busca o arquivo de respostas no caminho salvo em self.exercise
+
+        Raises:
+            FileNotFoundError: Se o arquivo de respostas não for encontrado no caminho local
+            RuntimeError: Se o método for chamado quando local=False
+
+        Returns:
+            str: Caminho do arquivo de respostas
+        """
         if self.local:
             local_path = Path(self.exercise) / settings.get_config("answers_file_name")
             if not local_path.is_file():
                 raise FileNotFoundError(f"O arquivo de respostas não foi encontrado em '{local_path}'")
             self.file = open(local_path, 'r', encoding='utf-8')
             return str(local_path)
+        raise RuntimeError("O método só pode ser usado quando local=True")
 
 
-    def get_content(self):
+    def get_content(self) -> str:
+        """Lê o conteúdo do arquivo
+
+        Raises:
+            ValueError: O arquivo de correção não existe
+
+        Returns:
+            str: Conteúdo do arquivo
+        """
         if not self.file:
             raise ValueError("o arquivo de correção não existe")
 
@@ -65,6 +101,15 @@ class Fetcher:
         return self.file.read()
 
     def get_decoded_json(self):
+        """Transforma o JSON do arqivo salvo em objeto python
+
+        Raises:
+            ValueError: O arquivo de correção não existe
+            RuntimeError: Falha ao decodificar o arquivo de correção
+
+        Returns:
+            _type_: Objeto python representando o JSON do arquivo
+        """
         if not self.file:
             raise ValueError("o arquivo de correção não existe")
 
@@ -78,5 +123,6 @@ class Fetcher:
 
 
     def cleanup(self):
+        """Deleta o arquivo temporário criado para o arquivo de respostas, caso exista."""
         if self.file != None:
             self.file.close()
