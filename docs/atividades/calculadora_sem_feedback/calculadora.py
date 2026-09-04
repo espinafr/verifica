cf0dece4f97edcb6f5f185935a360a18e5c049af
espinafr <theo.ruela@gmail.com>
2026-09-04 16:58:08 -0300

    Adiciona atividade sem feedback e com regex

---
+++ docs/atividades/calculadora_sem_feedback/calculadora.py
@@ -0,0 +1,7 @@
+# Solução do exercício de exemplo
+# espinafr/verifica/master/docs/atividades/calculadora_sem_feedback
+
+num1 = int(input("Número 1: "))
+num2 = int(input("Número 2: "))
+
+print(num1 + num2) # Caso essa soma dê errado o programa não vai mostrar o motivo
